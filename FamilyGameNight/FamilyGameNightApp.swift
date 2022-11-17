92d26c54e5f0902f758ecc9aeb2d6269caa1c889
metteghijsen <mette.ghijsen@gmail.com>
2022-11-17 10:39:32 +0100

    Initial Commit

---
+++ FamilyGameNight/FamilyGameNightApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamilyGameNightApp.swift
+//  FamilyGameNight
+//
+//  Created by Mette Ghijsen on 17/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct FamilyGameNightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
